488ef381569af6042d51cd9616c536cbd1e084fd
WillianBarata <willianbarata@gmail.com>
2020-01-15 23:00:02 -0300

    Testando a primeira classe de domínio

--- src/main/java/com/willian/mobile/resources/CategoriaResources.java
+++ src/main/java/com/willian/mobile/resources/CategoriaResources.java
@@ -1,15 +1,28 @@
 package com.willian.mobile.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.willian.mobile.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando !!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
