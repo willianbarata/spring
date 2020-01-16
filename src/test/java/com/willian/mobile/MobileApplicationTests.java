3fd5262baab195fb6ec32242a43a61593030696d
WillianBarata <willianbarata@gmail.com>
2020-01-15 22:10:03 -0300

    Projeto Criado

---
+++ src/test/java/com/willian/mobile/MobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willian.mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
