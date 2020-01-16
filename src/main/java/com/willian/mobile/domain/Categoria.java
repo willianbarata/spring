7a1d25b08e8b397932876a9174ae4a717c8e8269
WillianBarata <willianbarata@gmail.com>
2020-01-15 23:29:19 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/willian/mobile/domain/Categoria.java
+++ src/main/java/com/willian/mobile/domain/Categoria.java
@@ -2,9 +2,18 @@ package com.willian.mobile.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// mapeamento JPA
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //definindo estratégia de geração automática dos id
 	private Integer id;
 	private String nome;
 	
