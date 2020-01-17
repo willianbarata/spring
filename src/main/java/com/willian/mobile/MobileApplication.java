650dc7936746b330480ffa2641e8e52c758c0bf2
WillianBarata <willianbarata@gmail.com>
2020-01-16 23:15:30 -0300

    Estado e Cidade

--- src/main/java/com/willian/mobile/MobileApplication.java
+++ src/main/java/com/willian/mobile/MobileApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.willian.mobile.domain.Categoria;
+import com.willian.mobile.domain.Cidade;
+import com.willian.mobile.domain.Estado;
 import com.willian.mobile.domain.Produto;
 import com.willian.mobile.repositories.CategoriaRepository;
+import com.willian.mobile.repositories.CidadeRepository;
+import com.willian.mobile.repositories.EstadoRepository;
 import com.willian.mobile.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class MobileApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MobileApplication.class, args);
@@ -41,8 +51,23 @@ public class MobileApplication implements CommandLineRunner{
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais"); 
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
 	}
 
 	
