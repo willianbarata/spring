84bf552b9d15a517ab52369022acbcc14766ae17
WillianBarata <willianbarata@gmail.com>
2020-01-27 20:34:05 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/willian/mobile/resources/CategoriaResources.java
+++ src/main/java/com/willian/mobile/resources/CategoriaResources.java
@@ -1,6 +1,8 @@
 package com.willian.mobile.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.willian.mobile.DTO.CategoriaDTO;
 import com.willian.mobile.domain.Categoria;
 import com.willian.mobile.services.CategoriaService;
 
@@ -48,4 +51,12 @@ public class CategoriaResources {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
 }
