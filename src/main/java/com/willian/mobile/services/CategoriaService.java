194dad6a6c0d4f5e7f6f45a6fb0bd19d2ea99e1f
WillianBarata <willianbarata@gmail.com>
2020-01-27 21:13:16 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/willian/mobile/services/CategoriaService.java
+++ src/main/java/com/willian/mobile/services/CategoriaService.java
@@ -5,12 +5,17 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.willian.mobile.domain.Categoria;
 import com.willian.mobile.repositories.CategoriaRepository;
 import com.willian.mobile.services.exception.DataIntegrityException;
 import com.willian.mobile.services.exception.ObjectNotFoundException;
+
+
 
 @Service
 public class CategoriaService {
@@ -46,5 +51,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repo.findAll(); 
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderby, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderby);
+		return repo.findAll(pageRequest);
 	}
 }
