6134e8465b2de9ec217ae85b5c6633f3650ea518
WillianBarata <willianbarata@gmail.com>
2020-01-27 22:26:50 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/willian/mobile/resources/exception/ValidationError.java
@@ -0,0 +1,27 @@
+package com.willian.mobile.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandartError{
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void setList(List<FieldMessage> list) {
+		this.errors = list;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+}
