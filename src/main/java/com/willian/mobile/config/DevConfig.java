fae8fe90f7dd72066d7de18e194a12f40a2972ac
WillianBarata <willianbarata@gmail.com>
2020-02-02 15:13:19 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/willian/mobile/config/DevConfig.java
@@ -0,0 +1,25 @@
+package com.willian.mobile.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.willian.mobile.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+		dbService.instantiateDatabase();
+		
+		return true;
+	}
+}
