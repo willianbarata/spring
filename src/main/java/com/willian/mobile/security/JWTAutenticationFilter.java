22de3036b8661f964147a48ab415a3ee0bf74e21
WillianBarata <willianbarata@gmail.com>
2020-02-08 10:35:24 -0300

    liberando cors para put e commit

--- src/main/java/com/willian/mobile/security/JWTAutenticationFilter.java
+++ src/main/java/com/willian/mobile/security/JWTAutenticationFilter.java
@@ -60,6 +60,7 @@ public class JWTAutenticationFilter extends UsernamePasswordAuthenticationFilter
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
         res.addHeader("Authorization", "Bearer " + token);
+        res.addHeader("access-control-expose-headers", "Authorization");
 	}
 	
 	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
--- src/main/java/com/willian/mobile/security/JWTAuthorizationFilter.java
+++ src/main/java/com/willian/mobile/security/JWTAuthorizationFilter.java
@@ -31,7 +31,7 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter{
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException{
 		String header = request.getHeader("Authorization");
 		if(header != null && header.startsWith("Bearer")) {
-			UsernamePasswordAuthenticationToken auth = getAuthentication(request, header.substring(7));
+			UsernamePasswordAuthenticationToken auth = getAuthentication(header.substring(7));
 			if(auth != null) {
 				SecurityContextHolder.getContext().setAuthentication(auth);
 			}
