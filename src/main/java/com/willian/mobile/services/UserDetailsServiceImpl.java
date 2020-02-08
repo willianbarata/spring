48abef1ef459543633feae9d437c316d709fbbcd
WillianBarata <willianbarata@gmail.com>
2020-02-08 09:21:45 -0300

    tokon

---
+++ src/main/java/com/willian/mobile/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.willian.mobile.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.willian.mobile.domain.Cliente;
+import com.willian.mobile.repositories.ClienteRepository;
+import com.willian.mobile.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(cli.getId(), cli.getEmail(),cli.getSenha(), cli.getPerfis());
+	}
+
+}
